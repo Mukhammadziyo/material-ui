c3ce20638fb1dfa6a829446c194b9ce79b2766a1
Mukhammadziyo <mukhammadziyo@users.noreply.github.com>
2025-04-16 16:28:49 +0500

    a

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -69,7 +69,7 @@ function Navbar() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            Material UI
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -151,7 +151,7 @@ function Navbar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="User Avatar" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="User Avatar" src="/static/images/avatar/10.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
